66a5a5062f0bbf4a2df681ac8e3a67c2586d8a49
raj-jadhav <jadxur@gmail.com>
2024-12-02 22:42:24 +0300

    Added Average Sales calculation program to the repo and compiled it

---
+++ AverageSales.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+ 
+ //12 Average sales calculation
+    int days = 30;
+    int totalSales = 60000;
+    float averageSales = totalSales/days;
+
+    printf("The average sales are UGX%0.1f\n\n", averageSales);
+
+ return 0;
+}
