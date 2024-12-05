2511d2ac17e846edef88e14701002aaa2c5dd905
Raj-Jadhav <jadxur@gmail.com>
2024-12-05 00:37:04 -0800

    Added Multiple customer revenue prog to repo and compiled it with an exe

---
+++ Multi-customer-rev.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    //16. Total revenue from multiple customers
+    int customer_1 = 200;
+    int customer_2 = 500;
+    int customer_3 = 300;
+    int Total_revenue = customer_1 + customer_2 + customer_3;
+
+    printf("The total revenue = UGX %d\n",Total_revenue);   
+ 
+ return 0;
+}
